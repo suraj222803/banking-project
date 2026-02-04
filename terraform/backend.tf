8a23e6fd2cfa233b0487d2384fe9eeecebc47914
suraj ugale <surajugale96K@gmail.com>
2026-02-04 15:28:02 +0530

    Update S3 bucket name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "banking-terraform-state-2000"
+    bucket         = "banking-terraform-state-212503042228"
     key            = "banking/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
