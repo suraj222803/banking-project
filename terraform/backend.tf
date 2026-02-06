93aad74c2a3db6d76c6157ccb9c5be46091182da
suraj ugale <surajugale96K@gmail.com>
2026-02-06 14:31:38 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "banking-terraform-state-2255413"
     key            = "banking/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
     encrypt        = true
+    use_lockfile  = true
   }
 }
