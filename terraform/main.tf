b9a9ca3c70764557144f4c3f67838cc90f537fd8
suraj ugale <surajugale96K@gmail.com>
2026-02-10 15:06:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   vpc_cidr      = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
-  vpc_name      = "banking-vpc"
+  availability_zones = var.availability_zones
 }
 
 
