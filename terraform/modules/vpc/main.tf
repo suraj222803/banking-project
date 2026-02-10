bdf42394ce63da59be5d4ab72fa8e19123b340c6
suraj ugale <surajugale96K@gmail.com>
2026-02-10 14:35:55 +0530

    Refactor VPC resource and subnet configurations

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,22 +1,34 @@
-resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "banking-vpc"
+    Name = var.vpc_name
   }
 }
 
+# Public subnets
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
+# Private subnets
 resource "aws_subnet" "private" {
   count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
 }
 
-
+# Data source to get AZs
+data "aws_availability_zones" "available" {}
