ff029ef50b217484a30fd77007c4b978b1da95d8
suraj ugale <surajugale96K@gmail.com>
2026-02-10 14:33:47 +0530

    Rename ECR repository variable and clean up config

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,16 +1,3 @@
 resource "aws_ecr_repository" "this" {
-  name = var.repo_name
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-
-  tags = {
-    Project = "banking-devops"
-    Env     = "prod"
-  }
+  name = var.ecr_repo_name
 }
