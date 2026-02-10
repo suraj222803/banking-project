66ebe02e31f90e22ae58353c6e35b1cbc03397bb
suraj ugale <surajugale96K@gmail.com>
2026-02-10 14:39:46 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "ecr_repo_url" {
+  value = module.ecr.repository_url
+}
