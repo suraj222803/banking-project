da077dd955734c85f5513cee2d2feb960ec45ff7
suraj ugale <surajugale96K@gmail.com>
2026-02-10 16:21:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source        = "./modules/vpc"
+  vpc_name           = "banking-vpc"
   vpc_cidr      = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
