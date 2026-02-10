1dbc612566739dfe5ea10607d098bd0aca5049ae
suraj ugale <surajugale96K@gmail.com>
2026-02-10 14:32:49 +0530

    Update outputs.tf

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,8 +1,15 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  value = aws_eks_cluster.eks_cluster.name
 }
 
-output "oidc_issuer_url" {
-  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
 
+output "cluster_arn" {
+  value = aws_eks_cluster.eks_cluster.arn
+}
+
+output "node_group_arn" {
+  value = aws_eks_node_group.node_group.arn
+}
