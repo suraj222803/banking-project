629b494e98c961af92a5d7b9e5da375568f41a55
suraj ugale <surajugale96K@gmail.com>
2026-02-10 14:36:22 +0530

    Refactor VPC outputs to include new subnet and security group IDs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,7 +1,15 @@
-output "private_subnets" {
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
-output "vpc_id" {
-  value = aws_vpc.this.id
+output "default_security_group_id" {
+  value = aws_vpc.main.default_security_group_id
 }
