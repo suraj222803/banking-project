849738bef44302eddb92c19e6a34675b535f040f
suraj ugale <surajugale96K@gmail.com>
2026-02-10 14:33:12 +0530

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,12 +1,61 @@
-variable "cluster_name" {
+variable "vpc_id" {
   type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
 }
 
 variable "private_subnets" {
   type = list(string)
 }
 
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.29"
+}
+
 variable "eks_role_arn" {
-  type        = string
-  description = "IAM role ARN for EKS cluster"
+  type = string
+}
+
+variable "node_instance_role_arn" {
+  type = string
+}
+
+variable "node_instance_types" {
+  type    = list(string)
+  default = ["t3.micro"]
+}
+
+variable "ami_id" {
+  type    = list(string)
+  default = ["ami-0b6c6ebed2801a5cb"]
+}
+
+variable "desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 3
+}
+
+variable "ssh_key_name" {
+  type = string
+}
+
+variable "source_security_group_ids" {
+  type = list(string)
 }
